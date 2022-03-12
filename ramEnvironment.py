cd5f8926dd03dbbaa2c28a7611301b241f3bdb25
Sly <cri21631@uvg.edu.gt>
2022-03-11 19:18:47 -0600

    ramEnvironment v1.1

--- ramEnvironment.py
+++ ramEnvironment.py
@@ -1,2 +1,21 @@
+from curses import flash
 import simpy
 
+CPU_INST = 3
+AVAILABLE_CPU = 1
+PROCESOS = 50
+TIEMPO = 10
+UNIDAD = 1
+RAM_CAPACITY = 100
+
+
+
+
+def procesos(env, nombre, ramCapacidad, ramNeeded, inst_req, tiempo, velocidad):
+
+    #NEW
+    yield env.timeout(tiempo)
+
+    tiempoInicial = env.now
+    print("%p necesita %c de RAM" % (nombre, ramNeeded))
+    
