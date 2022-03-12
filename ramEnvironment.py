99def9f8cf89123d082aed296a680eae3ca581b3
Sly <cri21631@uvg.edu.gt>
2022-03-11 19:46:34 -0600

    Creacion de READY y RUNNING v1

--- ramEnvironment.py
+++ ramEnvironment.py
@@ -8,9 +8,6 @@ TIEMPO = 10
 UNIDAD = 1
 RAM_CAPACITY = 100
 
-
-
-
 def procesos(env, nombre, ramCapacidad, ramNeeded, inst_req, tiempo, velocidad):
 
     #NEW
@@ -18,4 +15,33 @@ def procesos(env, nombre, ramCapacidad, ramNeeded, inst_req, tiempo, velocidad):
 
     tiempoInicial = env.now
     print("%p necesita %c de RAM" % (nombre, ramNeeded))
+    print("Tiempo en ejecucion: %t" % (tiempo))
+
+    #READY
+    yield ramCapacidad.get(ramNeeded)
+    print("%p utilizando %c de RAM" % (nombre, ramNeeded))
+
+    next_inst = 0
     
+    #RUNNING
+    while inst_req > inst:
+        with cpu.request() as req:
+            yield req
+
+            next_inst = inst_req - velocidad
+
+            if (next_inst >= velocidad):
+
+                ejecutado = velocidad
+            
+            else:
+
+                ejecutado = next_inst
+
+            print("Ejecutando %p - El CPU utiliza %i instrucciones" % (nombre, ejecutado))
+
+            yield env.timeout(ejecutado/velocidad)
+
+            print("Ejecutado %p - Instrucciones utilizadas: (%i/%d)" % (nombre, next_inst, inst_req))
+
+
